30c1b2f1d70332daaa1f7862d6931238bbfe025c
Adrián Agudo Bruno <aagudo@cifpfbmoll.eu>
2021-02-24 19:51:21 +0100

    soport multilingue añadido y form corregido

---
+++ resources/lang/es/tutorsForm.php
@@ -0,0 +1,9 @@
+<?php
+
+return[
+    "companyName"=>"Nombre empresa",
+    "docType"=>"Tipo de documento",
+    "dni"=>"DNI",
+    "tutorName"=>"Nombre Tutor",
+    "tutorSurname1"=>"Apellido Tutor 1"
+];
