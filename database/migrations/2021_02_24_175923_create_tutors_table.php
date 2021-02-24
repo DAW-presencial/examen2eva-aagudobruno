a684eae0fff6b970ae887cf703bb4b2b96324159
Adrián Agudo Bruno <aagudo@cifpfbmoll.eu>
2021-02-24 19:12:07 +0100

    migracion Tutors creada

---
+++ database/migrations/2021_02_24_175923_create_tutors_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTutorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tutors', function (Blueprint $table) {
+            $table->id();
+            $table->string('companyName');
+            $table->string('docType');
+            $table->string('dni', 9);
+            $table->string('tutorName');
+            $table->string('tutorSurname1');
+            $table->string('tutorSurname2');
+            $table->string('dniCountry');
+            $table->string('province');
+            $table->string('municipi');
+            $table->enum('status',['active', 'inactive']);
+            $table->char('tlf', 9);
+            $table->string('email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tutors');
+    }
+}
